1f17057ca1c0df2d2880723f3360337c6376be84
birevv <bimarevansa2@gmail.com>
2026-07-30 00:17:54 +0700

    Force HTTPS for Livewire

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use Filament\Support\Facades\FilamentTimezone;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         FilamentTimezone::set(function (): string {
             $user = Auth::user();
 
